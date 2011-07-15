a13c3b1c1ae1ea3e7c002970af016ef19dc1e32c
dougbradbury <doug@8thlight.com>
2011-07-15 12:08:59 -0500

    compiling

---
+++ cslim_vehicle/fixtures.c
@@ -0,0 +1,12 @@
+//
+//  fixtures.c
+//  cslim_vehicle
+//
+//  Created by Doug Bradbury on 4/29/11.
+//  Copyright 2011 8th Light. All rights reserved.
+//
+
+#include "CSlim/fixtures.h"
+SLIM_FIXTURES
+
+SLIM_END
